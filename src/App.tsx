0a4bd455107e2882e45fabe51c3e99fbd59f819f
cheshta122 <142312749+cheshta122@users.noreply.github.com>
2025-09-06 14:50:57 +0530

    update: latest changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "@/components/ThemeProvider";
-import Index from "./pages/Index";
+
+import HomePage from "./pages/Index";
+import PortfolioPage from "./pages/portfolio";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -17,7 +19,8 @@ const App = () => (
         <Sonner />
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Index />} />
+            <Route path="/" element={<HomePage />} />
+            <Route path="/portfolio" element={<PortfolioPage />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
